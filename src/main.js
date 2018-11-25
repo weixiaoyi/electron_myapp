a128477ec7fd1e4f34f2e68012ae1af2f380ba33
weixiaoyi <2278095567@qq.com>
2018-11-25 17:06:09 +0800

    打包工具换掉

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+const { app, BrowserWindow } = require('electron')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -17,7 +17,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/distSource/index.html`);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
