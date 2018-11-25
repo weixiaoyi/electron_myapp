c435eac57b7cdc827075bfd89b99ad3d0bbe3d35
weixiaoyi <2278095567@qq.com>
2018-11-25 17:12:10 +0800

    electron-reload生效

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow } = require('electron')
 
+require('electron-reload')(__dirname,{
+  electron: require('${__dirname}/../../node_modules/electron')
+});
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
